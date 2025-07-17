15180330cbeb0839ff9939de3a91459a26d4da59
deepakshrivas440 <deepakshrivas440@gmail.com>
2025-07-17 23:13:54 +0530

    complete countries projects

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+// import countriesData from '../countriesData'
+import CountryCard from './CountryCard'
+import CountriesListShimmer from './CountriesListShimmer'
+import countriesData from '../countriesData';
+
+export default function CountriesList({ query }) {
+
+  if (!countriesData.length) {
+    return <CountriesListShimmer />
+  }
+
+  return (
+    <>
+      <div className="countries-container">
+        {countriesData
+          .filter((country) =>
+            country.name.toLowerCase().includes(query) || country.region?.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name}
+                flag={country.flags.svg}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+                data={country}
+              />
+            )
+          })}
+      </div>
+    </>
+  )
+}
---
+++ src/hooks/useTheme.js
@@ -0,0 +1,3 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../Contexts/ThemeContext'
+export const useTheme = () => useContext(ThemeContext)  
